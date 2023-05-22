5f53054392a8c8074ec862aaa5f84438eaca95c6
Musaf Hanif <musafhanif97@gmail.com>
2023-05-23 02:58:50 +0500

    Second commit after the updation of tax logic

--- cal2.js
+++ cal2.js
@@ -1,12 +1,38 @@
 $(document).ready(function(){
     var total = 0
+    $("#range-s").val(500)
+    $('#valBox').val(500)
     $("#range-s").on('input', function(event){
         event.preventDefault()
         $(".amount").val(event.target.value)
-        total = Number(event.target.value)
+        
     });
+    $("#valBox").on('input', function(){
+        $("#range-s").val($("#valBox").val())
+    })
     $(".calculate").click(function(){
+        total = Number($("#range-s").val())
         var tax = total /100 * 10
-        $(".final-amount").text( tax)
+        if(total <= 9875){
+            $(".final-amount").text( tax)
+            $(".cal-value").text(total)
+            $("#tax-perc").text("10")
+            $("#taxac").text(tax)
+        }
+        else if(total > 9875 && total <= 40125){
+            tax = total /100 * 12
+            $(".final-amount").text( tax)
+            $(".cal-value").text(total)
+            $("#tax-perc").text("12")
+            $("#taxac").text(tax)
+        }
+        else if(total > 40125 ){
+            tax = total /100 * 22
+            $(".final-amount").text( tax)
+            $(".cal-value").text(total)
+            $("#tax-perc").text("22")
+            $("#taxac").text(tax)
+            
+        }
     })
   });
